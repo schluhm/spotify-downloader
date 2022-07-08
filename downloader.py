ef97d7cbe60a3f66498d35703349a524d4cf392b
schluhm <45656593+Schluhm@users.noreply.github.com>
2022-07-08 23:04:30 +0200

    Rebuild to work in 2022

--- downloader.py
+++ downloader.py
@@ -1,135 +1,34 @@
 import os
 import subprocess
-import sys
-import urllib
-import time
-import json
-import httplib2
+import urllib.request
 
-from pytube import YouTube
-from pytube.exceptions import DoesNotExist, AgeRestricted
-from apiclient.discovery import build
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
+from youtube_dl import YoutubeDL
 
-def create_google_secrets_file(google_secrets_path):
-    GOOGLE_SECRETS = CONFIG["GOOGLE_SECRETS"]
-    GOOGLE_SECRETS_FILE = open(google_secrets_path, "w+")
-    GOOGLE_SECRETS_FILE.write(json.dumps(GOOGLE_SECRETS))
-    GOOGLE_SECRETS_FILE.close()
+YDL_OPTIONS = {'noplaylist': 'True', 'format': 'bestaudio/best', 'outtmpl': 'E:/Music/%(title)s.%(ext)s', 'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4',  # one of avi, flv, mkv, mp4, ogg, webm
+    }]}
 
-config_file = open("config.json", "r")
-CONFIG = json.loads(config_file.read())
-config_file.close()
 
-GOOGLE_CONFIG = CONFIG["GOOGLE_CONFIG"]
-APP_CONFIG = CONFIG["APP_CONFIG"]
-if not os.path.exists(APP_CONFIG["google_secrets_location"]):
-    create_google_secrets_file(APP_CONFIG["google_secrets_location"])
+DOWNLOAD_PATH = "E:/Music/"
 
-DOWNLOAD_PATH = APP_CONFIG["download_path"]
 
-def get_authenticated_service(args):
-    flow = flow_from_clientsecrets(APP_CONFIG["google_secrets_location"],
-                                   scope=GOOGLE_CONFIG["youtube_read_write_ssl_scope"],
-                                   message=GOOGLE_CONFIG["missing_client_secrets_message"])
-
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
-    credentials = storage.get()
-
-    if credentials is None or credentials.invalid:
-        credentials = run_flow(flow, storage, args)
-
-    return build(GOOGLE_CONFIG["api_service_name"], GOOGLE_CONFIG["api_version"], http=credentials.authorize(httplib2.Http()))
-
-args = argparser.parse_args()
-service = get_authenticated_service(args)
-
-def get_human_time(sec):
-    if sec >= 3600:  # Converts to Hours
-        return '{0:d} hour(s)'.format(int(sec / 3600))
-    elif sec >= 60:  # Converts to Minutes
-        return '{0:d} minute(s)'.format(int(sec / 60))
-    else:            # No Conversion
-        return '{0:d} second(s)'.format(int(sec))
-
-def bytes_to_str(bts):
-    bts = float(bts)
-    if bts >= 1024 ** 4:    # Converts to Terabytes
-        terabytes = bts / 1024 ** 4
-        size = '%.2fTb' % terabytes
-    elif bts >= 1024 ** 3:  # Converts to Gigabytes
-        gigabytes = bts / 1024 ** 3
-        size = '%.2fGb' % gigabytes
-    elif bts >= 1024 ** 2:  # Converts to Megabytes
-        megabytes = bts / 1024 ** 2
-        size = '%.2fMb' % megabytes
-    elif bts >= 1024:       # Converts to Kilobytes
-        kilobytes = bts / 1024
-        size = '%.2fKb' % kilobytes
-    else:                   # No Conversion
-        size = '%.2fb' % bts
-    return size
-
-class progressBar:
-    def __init__(self, barlength=25):
-        self.barlength = barlength
-        self.position = 0
-        self.longest = 0
-
-    def print_progress(self, cur, total, start):
-        currentper = cur / total
-        elapsed = int(time.clock() - start) + 1
-        curbar = int(currentper * self.barlength)
-        bar = '\r[' + '='.join(['' for _ in range(curbar)])  # Draws Progress
-        bar += '>'
-        bar += ' '.join(['' for _ in range(int(self.barlength - curbar))]) + '] '  # Pads remaining space
-        bar += bytes_to_str(cur / elapsed) + '/s '  # Calculates Rate
-        bar += get_human_time((total - cur) * (elapsed / cur)) + ' left'  # Calculates Remaining time
-        if len(bar) > self.longest:  # Keeps track of space to over write
-            self.longest = len(bar)
-            bar += ' '.join(['' for _ in range(self.longest - len(bar))])
-        sys.stdout.write(bar)
-
-    def print_end(self, *args):  # Clears Progress Bar
-        sys.stdout.write('\r{0}\r'.format((' ' for _ in range(self.longest))))
-
-def get_video(video_id):
-    try:
-        yt = YouTube("https://youtube.com/watch?v=" + video_id)
-        video = yt.get('mp4', '720p')
-        return True, video, yt.filename
-    except DoesNotExist:  # Sorts videos by resolution and picks the highest quality video if a 720p video doesn't exist
-        video = sorted(yt.filter("mp4"), key=lambda video: int(video.resolution[:-1]), reverse=True)[0]
-        return True, video, yt.filename
-    except AgeRestricted:
-        return False, None, ""
-
-def download_youtube_video(song_hash, song, video_ids, directory):
+def download_youtube_video(song_hash, song, video_id, directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
-    
-    found_video = False
-    video = None
-    filename = ""
-    i = 0
-    while not found_video:
-        found_video, video, filename = get_video(video_ids[i])
-        if found_video:
-            break
-        else:
-            i += 1
 
-    bar = progressBar()
-    print directory
-    video.download(directory, on_progress=bar.print_progress, on_finish=bar.print_end, force_overwrite = True)
-    os.rename(directory + "/" + filename + ".mp4", directory + "/" + song_hash + ".mp4")
-    return video_ids[i]
+    print(directory)
+    url = 'https://youtube.com/watch?v=' + video_id
+    YDL_OPTIONS['outtmpl'] = 'E:/Music/' + song_hash + '.%(ext)s'
+    with YoutubeDL(YDL_OPTIONS) as ydl:
+        ydl.download(url_list=[url])
+    return video_id
+
 
 def get_image(song_hash, song, directory):
     link = song["images"][0]
-    urllib.urlretrieve(link, directory + "/" + song_hash + ".png")
+    urllib.request.urlretrieve(link, directory + "/" + song_hash + ".png")
+
 
 def process_video(song_hash, song, directory):
     get_image(song_hash, song, directory)
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import json
-import os
 import hashlib
 import traceback
 import spotipy
 from spotipy import util
+import json
+from youtube_dl import YoutubeDL
 from downloader import *
+from requests import get
 
 SPOTIFY_SECRETS_FILE = "spotify_client_secret.json"
+YDL_OPTIONS = {'noplaylist': 'True', 'format': 'bestaudio/best', 'outtmpl': 'E:/Music/%(title)s.%(ext)s', 'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4',  # one of avi, flv, mkv, mp4, ogg, webm
+    }]}
+
 
 def get_spotify_playlist(secrets):
     auth_token = util.prompt_for_user_token(secrets["username"],
                                             'playlist-read-private',
                                             client_id=secrets["client_id"],
                                             client_secret=secrets["client_secret"],
-                                            redirect_uri='https://localhost')
+                                            redirect_uri='https://localhost/')
 
     sp = spotipy.Spotify(auth=auth_token)
-    return sp, sp.user_playlists(secrets["username"])
+    return sp, sp.user_playlist(secrets["username"], secrets["playlistID"])
+
 
 ##Load the downloaded songs list, and check to see if any new songs have been created
 def read_songs_file(songfile):
@@ -29,12 +36,14 @@ def read_songs_file(songfile):
         songs = open(songfile, "w+")
         songs.write(json.dumps([]))
         songs.close()
-        return []   
+        return []
+
 
 def write_songs_file(songfile, songs):
     song_file = open(songfile, "w")
     song_file.write(json.dumps(songs))
     song_file.close()
+
 
 def song_missing(song, directory):
     if os.path.isfile(directory + "/" + song["name"] + ".mp3"):
@@ -42,73 +51,69 @@ def song_missing(song, directory):
     else:
         return True
 
-def lookup_song(track_name, artists):
-    #First try to search for the song by channel name
-    channel_results = service.search().list(q=artists[0], part="snippet", maxResults=1, type="channel").execute()
-    song_results = None
-    query = track_name + " by " + ",".join(artists)
-    
-    try:
-        if not channel_results:
-            artist_channel_id = channel_results["items"][0]["id"]["channelId"]
-            song_results = service.search().list(q=query, part="snippet", channelId=artist_channel_id, type="video").execute()
-            if not song_results:
-                song_results = service.search().list(q=query, part="snippet", type='video').execute()
-        else:
-            song_results = service.search().list(q=query, part="snippet", type='video').execute()
-    except:
-        song_results = service.search().list(q=query, part="snippet", type='video').execute()
 
-    return [video["id"]["videoId"] for video in song_results["items"]]
+def lookup_song(track_name, artists):
+    arg = track_name + " by " + ",".join(artists)
+    with YoutubeDL(YDL_OPTIONS) as ydl:
+        try:
+            print("Trying to get somethign")
+            get(arg)
+        except:
+            video = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+        else:
+            video = ydl.extract_info(arg, download=False)
+    return video["id"]
+
 
 def download_song(song, directory):
-    #Hash the value of song, in order to serve as a unique identifier
+    # Hash the value of song, in order to serve as a unique identifier
     song_hash = hashlib.md5(json.dumps(song).encode('utf-8')).hexdigest()
-    selected_video = download_youtube_video(song_hash, song, song["youtube_videoids"], directory)
+    selected_video = download_youtube_video(song_hash, song, song["youtube_videoid"], directory)
     song["youtube_videoid"] = selected_video
     process_video(song_hash, song, directory)
 
+
 def add_page_to_list(songlist, page):
     for track in page["items"]:
-        track      = track["track"]
-        album      = track["album"]
+        track = track["track"]
+        album = track["album"]
         album_name = album["name"]
-        images     = [x["url"] for x in album["images"]]
-        artists    = [x["name"] for x in track["artists"]]
+        images = [x["url"] for x in album["images"]]
+        artists = [x["name"] for x in track["artists"]]
         track_name = track["name"]
         track_data = {
-            "name" : track_name,
-            "album" : album_name,
-            "images" : images,
-            "artists" : artists,
-            "youtube_videoid" : "",
-            "youtube_videoids" : lookup_song(track_name, artists),
-            "downloaded" : False,
+            "name": track_name,
+            "album": album_name,
+            "images": images,
+            "artists": artists,
+            "youtube_videoid": lookup_song(track_name, artists),
+            "downloaded": False,
         }
         songlist.append(track_data)
         print("Collected metadata from Spotify for " + track_name + " by " + ",".join(artists))
 
-def get_playlist_songs_list(playlists, sp, secrets):
+
+def get_playlist_songs_list(playlist, sp, secrets):
     songs_list = []
-    for playlist in playlists["items"]:
-        tracks = sp.user_playlist(secrets["username"], playlist["id"], fields="tracks,next")["tracks"]
-        numtracks = tracks["total"]
+    tracks = sp.user_playlist(secrets["username"], playlist["id"], fields="tracks,next")["tracks"]
+    numtracks = tracks["total"]
+    add_page_to_list(songs_list, tracks)
+    while tracks["next"]:
+        tracks = sp.next(tracks)
         add_page_to_list(songs_list, tracks)
-        while tracks["next"]:
-            tracks = sp.next(tracks)
-            add_page_to_list(songs_list, tracks)
-    
     print("Processed " + str(len(songs_list)) + " songs")
     return songs_list
 
+
 def download_missing_songs(songfile, secrets):
+    print("here we go")
     listed_songs = read_songs_file(songfile)
     distilled_list = [[x["name"], x["artists"]] for x in listed_songs]
-    try: 
-        sp, playlists = get_spotify_playlist(secrets)
-        playlist_songs = get_playlist_songs_list(playlists, sp, secrets)
-
-        print("STARTING THE DOWNLOAD PROCESS")
+    try:
+        print("really trying here")
+        sp, playlist = get_spotify_playlist(secrets)
+        print("Getting authenticated")
+        playlist_songs = get_playlist_songs_list(playlist, sp, secrets)
         for song in playlist_songs:
             if [song["name"], song["artists"]] not in distilled_list:
                 print("Downloading song: " + song["name"])
@@ -125,11 +130,12 @@ def download_missing_songs(songfile, secrets):
                 song["downloaded"] = True
                 print("Song downloaded")
     except Exception as e:
-        print traceback.format_exc()
+        print(traceback.format_exc())
     finally:
         for song in listed_songs:
             song.pop("youtube_videoids", None)
         write_songs_file(songfile, listed_songs)
+
 
 if __name__ == "__main__":
     config_file = open("config.json", "r")
@@ -137,6 +143,4 @@ if __name__ == "__main__":
     config_file.close()
 
     SPOTIFY_SECRETS = CONFIG["SPOTIFY_SECRETS"]
-
     download_missing_songs(CONFIG["APP_CONFIG"]["songs_file"], SPOTIFY_SECRETS)
-    
