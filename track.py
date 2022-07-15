9724b6777e9b8236f9f3b58321495f27b505084e
claasjg <claas.j.gramann@gmail.com>
2022-07-15 19:49:43 +0200

    Fix wrong function in __eq__

--- track.py
+++ track.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 class TrackInfo(namedtuple("TrackInfo", "id name album images artists disc_number track_number release_date")):
     def __eq__(self, other):
-        return self.id.__eq(other.id)
+        return self.id.__eq__(other.id)
 
     def __hash__(self):
         return self.id.__hash__()
