971beee2aeb45f20077fdb094fda94efcad35df7
ClaasJG <claas.j.gramann@gmail.com>
2022-08-16 22:34:27 +0200

    Upload plugin retry robustness

--- plugins/filebrowser_upload.py
+++ plugins/filebrowser_upload.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import click
 import requests
 from requests.utils import super_len
@@ -44,15 +46,13 @@ class FilebrowserUploadPlugin(Plugin):
     def __init__(self, **kwargs):
         self.api = kwargs[f"{self.__class__.__name__}_url"].strip().rstrip('/') + "/api"
         self.insecure = kwargs[f"{self.__class__.__name__}_insecure"]
-        self.token = self.get_token(
-            kwargs[f"{self.__class__.__name__}_username"],
-            kwargs[f"{self.__class__.__name__}_password"]
-        )
+        self.username = kwargs[f"{self.__class__.__name__}_username"]
+        self.password = kwargs[f"{self.__class__.__name__}_password"]
 
-    def get_token(self, username, password):
+    def get_token(self):
         try:
             response = requests.post(self.api + "/login", json={
-                "password": password, "recaptcha": "", "username": username,
+                "password": self.password, "recaptcha": "", "username": self.username,
             }, verify=not self.insecure)
             response.raise_for_status()
             return response.text
@@ -95,18 +95,32 @@ class FilebrowserUploadPlugin(Plugin):
         return option
 
     def on_track_done(self, track_path, track_name, track_status_cb):
-        url = self.api + "/resources/" + track_name
         fileobj = ProgressFile(open(track_path, 'rb'), track_status_cb)
+        token = self.get_token()
         headers = {
-            'X-Auth': self.token,  # version >= 2.0.3 seems use this header
-            'Authorization': 'Bearer {}'.format(self.token),  # version <= 2.0.0 seems use this header
+            'X-Auth': token,  # version >= 2.0.3 seems use this header
+            'Authorization': 'Bearer {}'.format(token),  # version <= 2.0.0 seems use this header
         }
 
-        with fileobj:
-            response = requests.post(
-                url, data=fileobj,
-                params={"override": False},
-                headers=headers,
-                verify=not self.insecure,
-            )
-            response.raise_for_status()
+        for i in range(3, -1, -1):
+            try:
+                with fileobj:
+                    response = requests.post(
+                        self.api + "/resources/" + track_name,
+                        data=fileobj,
+                        params={"override": True},
+                        headers=headers,
+                        verify=not self.insecure,
+                    )
+                    response.raise_for_status()
+                    break
+            except:
+                if 0 == i:
+                    raise
+                else:
+                    self.track_status_cb({
+                        'name': 'retry-upload',
+                        'total': 3,
+                        'progress': 4 - i
+                    })
+                    sleep(5)
